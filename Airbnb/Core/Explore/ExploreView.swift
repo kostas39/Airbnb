98c428fab7d75f324c8cc1316f3104199c4ca3c7
konstantinos koliolios <kolioliosk@gmail.com>
2024-04-09 00:06:13 +0100

    Initial Commit

---
+++ Airbnb/Core/Explore/ExploreView.swift
@@ -0,0 +1,29 @@
+//
+//  ExploreView.swift
+//  Airbnb
+//
+//  Created by Kostas Koliolios on 10/04/2024.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0 ... 10, id: \.self) { listing in
+                        Rectangle()
+                            .frame(height: 400)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
