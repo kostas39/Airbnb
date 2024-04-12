2712a8dc05189a0f7fed52cdcde12c758b6ad449
konstantinos koliolios <kolioliosk@gmail.com>
2024-04-12 13:43:17 +0100

    finish the custom search bar

--- Airbnb/Core/Explore/SearchAndFilterBar.swift
+++ Airbnb/Core/Explore/SearchAndFilterBar.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct SearchAndFilterBar: View {
     var body: some View {
         HStack {
-            Image(systemName: "magnifyinglass")
+            Image(systemName: "magnifyingglass")
                 
             VStack(alignment: .leading, spacing: 2) {
                 Text("Where to?")
@@ -28,6 +28,15 @@ struct SearchAndFilterBar: View {
                     .foregroundStyle(.black)
             })
         }
+        .padding(.horizontal)
+        .padding(.vertical, 10)
+        .overlay {
+            Capsule()
+                .stroke(lineWidth: 0.5)
+                .foregroundStyle(Color(.systemGray4))
+                .shadow(color: .black.opacity(0.4), radius: 2)
+        }
+        .padding()
     }
 }
 
