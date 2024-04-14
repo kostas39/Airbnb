a74cc7da5c44ee685bbbc65dfbd3539417188efc
konstantinos koliolios <kolioliosk@gmail.com>
2024-04-14 11:52:48 +0100

    listingdetailview

--- Airbnb/Core/Listings/ListingDetailView.swift
+++ Airbnb/Core/Listings/ListingDetailView.swift
@@ -18,7 +18,67 @@ struct ListingDetailView: View {
         ScrollView {
             ListingImageCarouselView()
                 .frame(height: 320)
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Miami Villa")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.semibold)
+                VStack(alignment: .leading) {
+                    
+                    HStack(spacing: 2) {
+                        Image(systemName: "star.fill")
+                        
+                        Text("4.9")
+                        
+                        Text(" - ")
+                        
+                        Text("28 reviews")
+                            .underline()
+                            .fontWeight(.semibold)
+                    }
+                    
+                    .foregroundStyle(.black)
+                    
+                    Text("Miami, Florida")
+                }
+                .font(.caption)
+            }
+            .padding(.leading)
+            .frame(maxWidth: .infinity,alignment: .leading)
+            
+            Divider()
+            
+            //host info view
+            HStack{
+                VStack(alignment: .leading, spacing: 4){
+                    Text("Entire villa hosted by Kostas Koliolios")
+                        .font(.headline)
+                        .frame(width: 250, alignment: .leading)
+                    
+                    HStack(spacing: 2) {
+                        Text("4 guests -")
+                        Text("4 bedrooms -")
+                        Text("4 beds -")
+                        Text("3 baths")
+
+                    }
+                    .font(.caption)
+                }
+                .frame(width: 300, alignment: .leading)
+                
+                Spacer()
+                
+                Image("my-photo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 64, height: 64)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            }
+            .padding()
+            
+            Divider()
         }
+        
     }
 }
 
