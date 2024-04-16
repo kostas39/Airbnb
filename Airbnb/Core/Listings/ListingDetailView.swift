a86531baebf89482af7adf4525e0ebf14b063a4e
konstantinos koliolios <kolioliosk@gmail.com>
2024-04-16 13:12:57 +0100

    bedrooms view

--- Airbnb/Core/Listings/ListingDetailView.swift
+++ Airbnb/Core/Listings/ListingDetailView.swift
@@ -100,6 +100,34 @@ struct ListingDetailView: View {
             }
             .padding()
             
+            Divider()
+            
+            //bedrooms view
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Where you'll sleep")
+                    .font(.headline)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 16) {
+                        ForEach(1 ..< 5) { bedroom in
+                            VStack {
+                                Image(systemName: "bed.double")
+                                
+                                Text("Bedroom \(bedroom)")
+                            }
+                            .frame(width:132, height: 100)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(lineWidth: 1)
+                                    .foregroundStyle(.gray)
+                            }
+                        }
+                    }
+                }
+                .scrollTargetBehavior(.paging)
+            }
+            .padding()
+            
         }
         
     }
