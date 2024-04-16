0f8c199fd7b3ac8dc37b1a1f4c7a5f37a08bee81
konstantinos koliolios <kolioliosk@gmail.com>
2024-04-16 12:54:03 +0100

    making the superhost section

--- Airbnb/Core/Listings/ListingDetailView.swift
+++ Airbnb/Core/Listings/ListingDetailView.swift
@@ -77,6 +77,28 @@ struct ListingDetailView: View {
             .padding()
             
             Divider()
+            
+            //listing features
+            
+            VStack(alignment: .leading, spacing: 16) {
+                ForEach(0 ..< 2) { feature in
+                    HStack(spacing: 12) {
+                        Image(systemName: "medal")
+                        
+                        VStack(alignment: .leading) {
+                            Text("Superhost")
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                            
+                            Text("Superhosts are experience, highly rated hosts who are commited to providing great stars for guests")
+                                .font(.caption)
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                }
+            }
+            .padding()
+            
         }
         
     }
