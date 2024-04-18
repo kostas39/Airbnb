f6385610b0084f6c7af57903e94d9f632330a536
konstantinos koliolios <kolioliosk@gmail.com>
2024-04-18 11:01:38 +0100

    destinationsearchview

---
+++ Airbnb/Core/Explore/DestinationSearchView.swift
@@ -0,0 +1,20 @@
+//
+//  DestinationSearchView.swift
+//  Airbnb
+//
+//  Created by Kostas Koliolios on 18/04/2024.
+//
+
+import SwiftUI
+
+struct DestinationSearchView: View {
+    var body: some View {
+        VStack {
+            Text("Where to?")
+        }
+    }
+}
+
+#Preview {
+    DestinationSearchView()
+}
