e3f69e172a5f23f754a8100231c20a56a12876ff
konstantinos koliolios <kolioliosk@gmail.com>
2024-04-19 16:41:17 +0100

    creating the listing model

---
+++ Airbnb/Core/Listings/Model/Listing.swift
@@ -0,0 +1,25 @@
+//
+//  Listing.swift
+//  Airbnb
+//
+//  Created by Kostas Koliolios on 19/04/2024.
+//
+
+import Foundation
+
+struct Listing: Identifiable, Codable {
+    let id: String
+    let ownerUid: String
+    let ownerName: String
+    let ownerImageUrl: String
+    let numberOfBedrooms: Int
+    let numberOfBathrooms: Int
+    let numberOfGuests: Int
+    let numberOfBeds: Int
+    var pricePerNight: Int
+    let latitude: Double
+    let longitude: Double
+    let address: String
+    let city: String
+    let state: String
+}
