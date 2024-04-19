0c118d5e58b5dcbfbbcfc9e3d2ada3571da38d3c
konstantinos koliolios <kolioliosk@gmail.com>
2024-04-19 15:51:44 +0100

    profile

--- Airbnb/Core/Profile/ProfileView.swift
+++ Airbnb/Core/Profile/ProfileView.swift
@@ -9,28 +9,48 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
+        VStack {
+            //profile login view
+            VStack(alignment: .leading, spacing: 32) {
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Profile")
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                    Text("Log in to start planning your next trip")
+
+                }
+                
+                
+                Button {
+                    print("Log in")
+                } label: {
+                    Text("Log in")
+                        .foregroundStyle(.white)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(width: 360, height: 48)
+                        .background(.pink)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                }
+                
+                HStack {
+                    Text("Don't have an account?")
+                    
+                    Text("Sign up")
+                        .fontWeight(.semibold)
+                        .underline()
+                }
+                .font(.caption)
+            }
             
-            VStack(alignment: .leading, spacing: 8) {
-                Text("Profile")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-                Text("Log in to start planning your next trip")
+            VStack(spacing:24) {
+                ProfileOptionRowView(imageName: "gear", title: "Setting")
+                ProfileOptionRowView(imageName: "gear", title: "Accessibility")
+                ProfileOptionRowView(imageName: "questionmark.circle", title: "Visit the help center")
 
             }
-            
-            
-            Button {
-                print("Log in")
-            } label: {
-                Text("Log in")
-                    .foregroundStyle(.white)
-                    .font(.subheadline)
-                    .fontWeight(.semibold)
-                    .frame(width: 360, height: 48)
-                    .background(.pink)
-                    .clipShape(RoundedRectangle(cornerRadius: 8))
-            }
+            .padding(.vertical)
         }
     }
 }
